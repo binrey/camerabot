f672c6d7387528b5feb00919006bd00df061dfe1
Andrey Rybin <RybinAV6.transport.mos.ru>
2025-07-30 17:12:18 +0300

    improve: add obstacle avoidare and camera

--- src/my_package/launch/robot_with_rviz_launch.py
+++ src/my_package/launch/robot_with_rviz_launch.py
@@ -10,6 +10,7 @@ from webots_ros2_driver.webots_controller import WebotsController
 def generate_launch_description():
     package_dir = get_package_share_directory('my_package')
     robot_description_path = os.path.join(package_dir, 'resource', 'my_robot.urdf')
+    rviz_config_path = os.path.join(package_dir, 'resource', 'my_robot.rviz')
 
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'my_world.wbt')
@@ -27,14 +28,22 @@ def generate_launch_description():
         executable='obstacle_avoider',
     )
 
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+    )
+
     return LaunchDescription([
         webots,
         my_robot_driver,
         obstacle_avoider,
+        rviz,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
         )
-    ])
+    ]) 
--- src/my_package/setup.py
+++ src/my_package/setup.py
@@ -4,8 +4,10 @@ package_name = 'my_package'
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_with_rviz_launch.py']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
 data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.rviz']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
@@ -23,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'my_robot_driver = my_package.my_robot_driver:main',
+            'obstacle_avoider = my_package.obstacle_avoider:main',
         ],
     },
 )
